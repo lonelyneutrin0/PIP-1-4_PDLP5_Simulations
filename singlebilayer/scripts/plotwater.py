7a25477c6e3dfb5a0e69cb1194623d53530aa044
hkbel <belagal1@msu.edu>
2025-06-03 22:44:34 -0400

    Water analysis scripts,see #2

--- singlebilayer/scripts/plotwater.py
+++ singlebilayer/scripts/plotwater.py
@@ -1,112 +1,200 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+
+from numpy.typing import NDArray
+from typing import Tuple
+
 plt.rcParams.update({
     "text.usetex": True,
-    "font.family": "sans-serif"
+    "font.family": "sans-serif",
+    "font.sans-serif": ['Arial', 'Helvetica', 'DejaVu Sans']
 })
-plt.rcParams['font.sans-serif'] = ['Arial', 'Helvetica', 'DejaVu Sans']
 
 
-def digitizewater(data):
-	bins = np.array([-25,-2,12,25])
-	digitizedwater = np.digitize(data,bins)
-	#Timestep is the first axis, second axis is for a specific water molecule.
-	return digitizedwater
-#Get the same array as the input, but now without duplicates so it is easier to look at. https://stackoverflow.com/questions/37839928/remove-consecutive-duplicates-in-a-numpy-array
-def filternorepeats(arr):
-	#Filter out odd numbers.
-	eventimes = np.nonzero(arr%2==0)[0]
-	arr = arr[arr%2==0]//2
-	diff = np.diff(arr)
-	return (arr[np.insert(diff.astype(bool), 0, True)], np.hstack([0, eventimes[1+np.nonzero(diff)[0]]]))
+def digitize_water(data: NDArray) -> NDArray:
+    """
+    Digitize input Z-coordinate data into bins.
 
-def norepeats(arr):
-	diff = np.diff(arr)
-	return (arr[np.insert(diff.astype(bool), 0, True)], np.hstack([0, 1+np.nonzero(diff)[0]]))
+    Params:
 
-counter = 0
-def plotcrossing(data, start, stop):
-	global counter
-	if counter < 100:
-		fig, ax = plt.subplots(1,1)
-		ax.plot(data)
-		ax.axvspan(start,stop, color='gray')
-		fig.savefig("waterplots/plot_%d.png" % counter)
-		counter += 1
-def countcrossings2(digitized, data):
-	crossingtimes = []
-	crossingdurations = []
-	counter = 0
-	# print(digitized.shape)
-	# print(np.sum(digitized == 2))
-	# print(np.nonzero(digitized == 2))
-	# print(np.nonzero(digitized == 2)[1].shape)
-	#For whatever reason, np.nonzero will give you the same "j" axis multiple times. 
-	#We just want it once, so put it into a set.
-	jset = set(np.nonzero(digitized == 2)[1])
-	# print(len(jset))
-	# exit()
-	#We want a sequence for a specific water molecule where it is in the "1" bin for a bit, but has 0 and 2 as bookends.
-	for j in jset:
-		shortarray, times = filternorepeats(digitized[:,j])
-		oneidxs = np.nonzero(shortarray==1)[0]
-		for k in oneidxs:
-			if (k > 0) and (k < len(shortarray) - 1) and (shortarray[k-1] != shortarray[k+1]):
-				# print("We have a crossing! %d, %d" % (j, k))
-				#print(shortarray, times)
-				# print(shortarray, oneidxs)
-				crossingtimes.append(times[k+1])
-				crossingdurations.append(times[k+1]-times[k-1])
-				#plotcrossing(data[:,j],times[k-1],times[k+1])
-	return (np.array(crossingtimes), np.array(crossingdurations))
-def countcrossings(digitized, data):
-	crossingset = set()
-	crossingtimes = []
-	crossingdurations = []
-	jset = set(np.nonzero(digitized == 2)[1])
-	#We want a sequence for a specific water molecule where it is in the "1" bin for a bit, but has 0 and 2 as bookends.
-	for j in jset:
-		shortarray, times = norepeats(digitized[:,j])
-		oneidxs = np.nonzero(shortarray==2)[0]
-		for k in oneidxs:
-			l = 1
-			while k - l > 0 and not (shortarray[k-l] == 0 or shortarray[k-l] == 4):
-				l+=1
-			m = 1
-			while k+m < len(shortarray)-1 and not (shortarray[k+m] == 0 or shortarray[k+m] == 4):
-				m+=1
-			if (k - l  >= 0) and (k + m <= len(shortarray) - 1) and (shortarray[k-l] != shortarray[k+m]) and (shortarray[k+m] == 0 or shortarray[k+m] == 4) and (shortarray[k-l] == 0 or shortarray[k-l] == 4) and (j,k+m) not in crossingset:
-				#print("We have a crossing! %d, %d" % (j, k))
-				crossingset.add((j,k+m))
-				crossingtimes.append(times[k+m])
-				crossingdurations.append(times[k+m]-times[k-l])
-				plotcrossing(data[:,j],times[k-l],times[k+m])
-	
-	return (np.array(list(crossingtimes)), np.array(crossingdurations))
+    data : NDArray
+        NumPy array containing Z coordinates of water molecules.
+
+    Returns:
+
+        NDArray containing the binned positions of water molecules.
+    """
+
+    bins = np.array([-25, -2, 12, 25])
+    return np.digitize(data, bins)
 
 
-for d in ['porinalone', 'porinwithcap']:
-	rates = []
-	for i in range(3):
-		data = np.load("waterdata/%s_%d.npy" % (d, i))
-		#Frames are 100ps apart.
-		simtime = 0.1 * len(data) #This is now in ns.
-		digitzeddata = digitizewater(data)
-		#print(checkcrossings(digitzeddata))
-		# start = time.time()
-		# times, durations = countcrossings(digitzeddata, data)
-		# #print(d, i, times, durations, len(times))
-		# rate = len(times) / simtime
-		# print(d,i,len(times), simtime, rate)
-		# print(time.time() - start)
-		start = time.time()
-		times, durations = countcrossings2(digitzeddata, data)
-		#print(d, i, times, durations, len(times))
-		rate = len(times) / simtime
-		rates.append(rate)
-		print(d,i,len(times), simtime, rate)
-		print(time.time() - start)
-		np.savez("waterdata/reduced%s_%d.npz", time=times, duration=durations)
-	rates = 1000 * np.array(rates) # Multiplying by 1000 puts it in waters/microsecond
-	print(np.mean(rates), np.std(rates) / np.sqrt(3))
+def filter_no_repeats(arr: NDArray) -> Tuple[NDArray, NDArray]:
+    """
+    Filter out consecutive duplicates from array, considering only even values.
+
+    Params:
+
+    arr : NDArray
+        Input array.
+
+    Returns
+
+    Tuple of filtered array and corresponding indices.
+    """
+
+    event_indices = np.nonzero(arr % 2 == 0)[0]
+    filtered_arr = arr[arr % 2 == 0] // 2
+    diff = np.diff(filtered_arr)
+
+    unique_vals = filtered_arr[np.insert(diff.astype(bool), 0, True)]
+    unique_indices = np.hstack([0, event_indices[1 + np.nonzero(diff)[0]]])
+    return unique_vals, unique_indices
+
+
+def no_repeats(arr: NDArray) -> Tuple[NDArray, NDArray]:
+    """
+    Remove consecutive duplicates from the array.
+
+    Params:
+
+    arr : NDArray
+        Input array.
+
+    Returns:
+
+    Tuple of filtered array and corresponding indices.
+    """
+    diff = np.diff(arr)
+    unique_vals = arr[np.insert(diff.astype(bool), 0, True)]
+    unique_indices = np.hstack([0, 1 + np.nonzero(diff)[0]])
+    return unique_vals, unique_indices
+
+
+def plot_crossing(data: NDArray, start: int, stop: int):
+    """
+    Plot a single crossing event and save it to a PNG file.
+
+    Params:
+
+    data : NDArray
+        Time series data of a single water molecule.
+    start : int
+        Start index of the crossing event.
+    stop : int
+        End index of the crossing event.
+    """
+    if not hasattr(plot_crossing, "counter"):
+        plot_crossing.counter = 0
+
+    if plot_crossing.counter < 100:
+        fig, ax = plt.subplots()
+        ax.plot(data)
+        ax.axvspan(start, stop, color='gray', alpha=0.3)
+        fig.savefig(f"plot_{plot_crossing.counter}.png")
+        plt.close(fig)
+        plot_crossing.counter += 1
+
+
+def count_crossings2(digitized: NDArray, data: NDArray) -> Tuple[NDArray, NDArray]:
+    """
+    Count crossing events where a water molecule moves into bin '1' surrounded by bins '0' and '2'.
+
+    Params:
+
+    digitized : NDArray
+        Digitized bin data for all molecules over time.
+    data : NDArray
+        Original water coordinate data (used for plotting).
+
+    Returns:
+
+    Tuple of crossing start times and crossing durations.
+    """
+    crossing_times = []
+    crossing_durations = []
+
+    # Find all unique molecules that are in bin 2 at some time
+    molecules_with_bin2 = set(np.nonzero(digitized == 2)[1])
+
+    for molecule_idx in molecules_with_bin2:
+        filtered_bins, times = filter_no_repeats(digitized[:, molecule_idx])
+        one_indices = np.nonzero(filtered_bins == 1)[0]
+
+        for idx in one_indices:
+            # Check if the bin 1 is bookended by different bins
+            if 0 < idx < len(filtered_bins) - 1:
+                if filtered_bins[idx - 1] != filtered_bins[idx + 1]:
+                    crossing_times.append(times[idx + 1])
+                    crossing_durations.append(times[idx + 1] - times[idx - 1])
+
+    return np.array(crossing_times), np.array(crossing_durations)
+
+
+def count_crossings(digitized: NDArray, data: NDArray) -> Tuple[NDArray, NDArray]:
+    """
+    Alternate method to count crossing events
+
+    Params:
+
+    digitized : NDArray
+        Digitized bin data for all molecules over time.
+    data : NDArray
+        Original water coordinate data (used for plotting).
+
+    Returns:
+
+    Tuple of crossing start times and crossing durations.
+    """
+    crossings_set = set()
+    crossing_times = []
+    crossing_durations = []
+    molecules_with_bin2 = set(np.nonzero(digitized == 2)[1])
+
+    for molecule_idx in molecules_with_bin2:
+        filtered_bins, times = no_repeats(digitized[:, molecule_idx])
+        two_indices = np.nonzero(filtered_bins == 2)[0]
+
+        for idx in two_indices:
+            left_offset = 1
+            while idx - left_offset > 0 and filtered_bins[idx - left_offset] not in (0, 4):
+                left_offset += 1
+
+            right_offset = 1
+            while idx + right_offset < len(filtered_bins) - 1 and filtered_bins[idx + right_offset] not in (0, 4):
+                right_offset += 1
+
+            left_idx = idx - left_offset
+            right_idx = idx + right_offset
+
+            if (left_idx >= 0 and right_idx <= len(filtered_bins) - 1 and
+                filtered_bins[left_idx] != filtered_bins[right_idx] and
+                filtered_bins[right_idx] in (0, 4) and filtered_bins[left_idx] in (0, 4) and
+                (molecule_idx, right_idx) not in crossings_set):
+                
+                crossings_set.add((molecule_idx, right_idx))
+                crossing_times.append(times[right_idx])
+                crossing_durations.append(times[right_idx] - times[left_idx])
+                plot_crossing(data[:, molecule_idx], times[left_idx], times[right_idx])
+
+    return np.array(crossing_times), np.array(crossing_durations)
+
+
+def main():
+    data = np.load(f"watz.npy")
+    sim_time_ns = 0.1 * len(data)  # each frame is 100ps, convert to ns
+
+    digitized_data = digitize_water(data)
+
+    start_time = time.time()
+    times, durations = count_crossings2(digitized_data, data)
+    rate = len(times) / sim_time_ns
+
+    print(f"Crossings={len(times)}, Simulation Time={sim_time_ns:.2f} ns, rate={rate:.4f} crossings/ns")
+    print(f"Processing time: {time.time() - start_time:.2f} s")
+
+    np.savez(f"reduced.npz", time=times, duration=durations)
+
+if __name__ == "__main__":
+    main()
