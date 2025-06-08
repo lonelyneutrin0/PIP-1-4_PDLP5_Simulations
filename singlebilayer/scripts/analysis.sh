cc6bac7420824190ab76714a6976257a5a898c57
hkbel <belagal1@msu.edu>
2025-06-07 21:29:58 -0400

    Update analysis.sh

--- singlebilayer/scripts/analysis.sh
+++ singlebilayer/scripts/analysis.sh
@@ -1,4 +1,12 @@
+vmd-python() {
+    if [ -z "$1" ]; then
+        echo "Usage: vmdpy <script.py> [args...]"
+        return 1
+    fi
+    vmd -dispdev text -python -e "$1" "${@:2}"
+}
+
 vmd-python trackwater.py 
 venvit 
-python plotwater.py
+python plotwater.py > analysis_run.log
 deactivate
