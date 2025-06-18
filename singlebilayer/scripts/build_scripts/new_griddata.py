97a8853f0207f3bb39afd735436968c322610a7f
hkbel <belagal1@msu.edu>
2025-06-18 19:22:58 -0400

    Scripts rework

--- singlebilayer/scripts/build_scripts/new_griddata.py
+++ singlebilayer/scripts/build_scripts/new_griddata.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 # Grid shape to match the working one
-shape = (2, 2, 123) # Z - 40
+shape = (2, 2, 171) # Z - 20
 data = np.zeros(shape).reshape(-1, 3)
 
 # Origin and spacing from your valid PME file
-origin = np.array([-76.172, -77.492, -62.226]) # ADD THE ORIGIN OF THE PERIODIC CELL HERE
+origin = np.array([-76.172, -77.492, -83.226]) # ADD THE ORIGIN OF THE PERIODIC CELL HERE
 delta = np.array([10.0, 10.0, 1.0])
 
 header_str = f"""object 1 class gridpositions counts {shape[0]} {shape[1]} {shape[2]}
